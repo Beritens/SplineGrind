70ae405c9416c9f7e96a1307210f79247479e33d
Ben <richterneb@gmail.com>
2025-08-19 00:50:01 +0200

    init
    (cherry picked from commit adc4bdc746fc1855298aa2b394c82221bc7a18a4)

---
+++ src/main.rs
@@ -0,0 +1,165 @@
+mod spines_plugin;
+mod physics_plugin;
+
+use bevy::math::ops::sin;
+use bevy::{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    prelude::*,
+    text::FontSmoothing,
+};
+
+use nalgebra::{Vector, Vector2};
+use rand::Rng;
+use crate::physics_plugin::{Gravitate, PhysicsPlugin, VerletObject};
+use crate::spines_plugin::{Position, SplinePlugin};
+
+struct OverlayColor;
+
+impl OverlayColor {
+    const RED: Color = Color::srgb(1.0, 0.0, 0.0);
+    const GREEN: Color = Color::srgb(0.0, 1.0, 0.0);
+}
+fn main() {
+    let mut app = App::new();
+    app.add_plugins((
+            DefaultPlugins,
+            SplinePlugin,
+            PhysicsPlugin,
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
+                    text_config: TextFont {
+                        // Here we define size of our overlay
+                        font_size: 12.0,
+                        // If we want, we can use a custom font
+                        font: default(),
+                        // We could also disable font smoothing,
+                        font_smoothing: FontSmoothing::default(),
+                        ..default()
+                    },
+                    // We can also change color of the overlay
+                    text_color: OverlayColor::GREEN,
+                    // We can also set the refresh interval for the FPS counter
+                    refresh_interval: core::time::Duration::from_millis(100),
+                    enabled: true,
+                },
+            },
+        ));
+    app.insert_resource(Time::<Fixed>::from_seconds(0.01666666));
+    app.add_systems(Startup, setup);
+    app.run();
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2d);
+
+    let circle = meshes.add(Circle::new(30.0));
+    let small_circle = meshes.add(Circle::new(1.0));
+    let rec = meshes.add(Rectangle::new(1.0, 10.0));
+    let color = Color::WHITE;
+    let material = materials.add(color);
+
+    // spline
+    let mut splines:Vec<Entity> = Vec::with_capacity(100);
+
+    for i in 0..1{
+
+        splines.push(commands.spawn((crate::spines_plugin::Spline())).id());
+    }
+    let mut rng = rand::thread_rng();
+
+    commands.spawn((Position(Vector2::new(0.0, 1000.0)),
+                    crate::spines_plugin::Pusher(),
+                    crate::spines_plugin::FollowMouse(),
+                    // Transform::from_xyz(
+                    //     0.0,
+                    //     0.0,
+                    //     0.0,
+                    // ),
+                    // Mesh2d(circle.clone()),
+                    // MeshMaterial2d(material.clone()),
+    ));
+
+    for i in 0..100{
+        // let x_rand: f32 = rng.gen_range(-10.0..10.0);
+        let x_rand: f32 = 0.0;
+        let x =  -800.0 + i as f32 * 20.0 + x_rand;
+        // let y: f32 = rng.gen_range(-5.0..5.0);
+        let y: f32 = sin(x*0.01)*100.0;
+
+        for j in 0..1{
+
+            commands.spawn((Position(Vector2::new(x,y)),
+                            crate::spines_plugin::Target(Vector2::new(x, y+(j as f32*5.0))),
+                            crate::spines_plugin::Movable {default_position: Vector2::new(x, y)},
+                            // Transform::from_xyz(
+                            //     x,
+                            //     y,
+                            //     0.0,
+                            // ),
+                            // Mesh2d(circle.clone()),
+                            // MeshMaterial2d(material.clone()),
+                            crate::spines_plugin::ControlPoint(splines[j]),
+
+            ));
+
+
+        }
+
+    }
+
+
+    for i in 0..1000 {
+
+        for j in 0..1{
+
+            commands.spawn((Position(Vector2::new(5.0 * i as f32,0.0)),
+                            Transform::from_xyz(
+                                0.0,
+                                0.0,
+                                0.0,
+                            ),
+                            Mesh2d(small_circle.clone()),
+                            MeshMaterial2d(material.clone()),
+                            crate::spines_plugin::Visualization(splines[j])
+            ));
+
+
+        }
+
+
+    }
+
+    for i in 0..1 {
+
+        for j in 0..1{
+
+            commands.spawn((Position(Vector2::new(5.0 * i as f32,0.0)),
+                            Transform::from_xyz(
+                                0.0,
+                                0.0,
+                                0.0,
+                            ),
+                            Mesh2d(rec.clone()),
+                            MeshMaterial2d(material.clone()),
+                            crate::spines_plugin::VisualizationGradient(splines[j])
+            ));
+
+
+        }
+
+
+    }
+
+    commands.spawn((Position(Vector2::new(100.0, 300.0)),
+                    Transform::from_xyz(
+                        0.0,
+                        0.0,
+                        0.0,
+                    ),
+                    Mesh2d(circle.clone()),
+                    MeshMaterial2d(material.clone()),
+                    Gravitate(),
+                    VerletObject{position_old: Vector2::new(100.0,300.0), acceleration: Vector2::new(0.0,0.0)}
+    ));
+
+}
---
+++ src/spines_plugin.rs
@@ -0,0 +1,474 @@
+use bevy::math::ops::{atan2, sin};
+use bevy::{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    prelude::*,
+    text::FontSmoothing,
+};
+
+use nalgebra::{Vector, Vector2, Vector3};
+use rand::Rng;
+use roots::{find_root_newton_raphson, SimpleConvergency};
+use crate::physics_plugin::VerletObject;
+
+pub struct SplinePlugin;
+
+impl Plugin for SplinePlugin {
+    fn build(&self, app: &mut App) {
+
+        app.add_systems(Update, (render_spline, render_gradient, move_points, update_position, push, go_to_target));
+        app.add_systems(PostUpdate, (follow_mouse.after(TransformSystem::TransformPropagate)));
+    }
+}
+
+#[derive(Component, Debug, Clone)]
+pub struct Position(pub Vector2<f32>);
+
+#[derive(Component, Debug, Clone)]
+pub struct Moving();
+
+#[derive(Component)]
+pub struct FollowMouse();
+
+#[derive(Component)]
+pub struct Target(pub Vector2<f32>);
+
+#[derive(Component)]
+pub struct Movable{
+    pub default_position: Vector2<f32>
+}
+
+#[derive(Component)]
+pub struct Pusher();
+
+
+#[derive(Component)]
+pub struct Spline();
+
+#[derive(Component)]
+#[relationship(relationship_target = ControlledBy)]
+pub struct ControlPoint(pub Entity);
+
+#[derive(Component, Deref)]
+#[relationship_target(relationship = ControlPoint)]
+pub struct ControlledBy(Vec<Entity>);
+
+#[derive(Component)]
+#[relationship(relationship_target = VisualizedBy)]
+pub struct Visualization(pub Entity);
+
+#[derive(Component, Deref)]
+#[relationship_target(relationship = Visualization)]
+pub struct VisualizedBy(Vec<Entity>);
+
+#[derive(Component)]
+#[relationship(relationship_target = GradientVisualizedBy)]
+pub struct VisualizationGradient(pub Entity);
+
+#[derive(Component, Deref)]
+#[relationship_target(relationship = VisualizationGradient)]
+pub struct GradientVisualizedBy(Vec<Entity>);
+
+
+
+
+fn push(
+    mut pushed_query: Query<(&mut Position, &Target, &Movable), Without<Pusher>>,
+    pusher_query: Query<(&Position, &Pusher)>
+){
+    for (push_pos, pusher) in &pusher_query{
+
+        for (mut pushed_pos, mut target, mov) in &mut pushed_query{
+            if((pushed_pos.0 - push_pos.0).norm() <= 100.0){
+                pushed_pos.0 =pushed_pos.0 * 0.6 +  (push_pos.0 + (pushed_pos.0 - push_pos.0).normalize() * 100.0) * 0.4;
+            }
+            // else{
+            //     target.0 = mov.default_position
+            // }
+        }
+
+    }
+}
+
+fn go_to_target(
+    mut target_query: Query<(&mut Position, &Target)>,
+){
+
+    for (mut pos, target) in &mut target_query{
+        pos.0 = pos.0 * 0.9 + target.0 * 0.1;
+    }
+
+}
+fn update_position(
+    mut query: Query<(&Position, &mut Transform)>
+){
+    for (pos, mut transform) in query.iter_mut() {
+        transform.translation.x = pos.0.x;
+        transform.translation.y = pos.0.y;
+    }
+}
+
+fn follow_mouse(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    mut follower_query: Query<(&mut Position, &mut FollowMouse)>,
+    window: Query<&Window>,
+) {
+    let (camera, camera_transform) = *camera_query;
+    let Ok(window) = window.single() else {
+        return;
+    };
+
+    let Some(cursor_position) = window.cursor_position() else {
+        return;
+    };
+
+    // Calculate a world position based on the cursor's position.
+    let Ok(world_pos) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
+
+    for (mut pos, mut follower_mouse) in follower_query.iter_mut() {
+        pos.0.x = world_pos.x;
+        pos.0.y = world_pos.y;
+    }
+
+}
+
+fn move_points(
+    time: Res<Time>,
+    mut position_query: Query<(&mut Position, &mut Transform, &Moving)>,
+){
+    for (mut pos, mut trans, mov) in &mut position_query{
+        pos.0.y += 0.2 * sin(time.elapsed_secs()+ pos.0.x);
+        trans.translation.y = pos.0.y;
+    }
+
+}
+
+fn de_casteljau(mut control_points: Vec<Vector2<f32>>, t: f32) -> Vector2<f32> {
+    let mut r: i32 = 1;
+    while r < control_points.len() as i32 {
+        let n = control_points.len() as i32 - r;
+        for i in 0..n {
+            control_points[i as usize] = t * control_points[i as usize + 1] + (1.0 -t) * control_points[i as usize];
+        }
+        r += 1;
+    }
+
+    return control_points[0];
+}
+
+#[inline(always)]
+pub fn de_boors<const LEN: usize>(control_points: &Vec<Vector2<f32>>, t: f32, t_vec: &Vec<f32>,temp: &mut [Vector2<f32>; LEN], l: usize) -> Vector2<f32> {
+    //assume uniform knots
+    let N: usize = LEN - 1;
+
+    let t_scale = t * t_vec[t_vec.len() - 1];
+
+    let base = l - N;
+
+    temp.copy_from_slice(&control_points[base..=l]);
+
+
+    for r in 1..=N {
+        let ir_base = r + base;        // i+r+(l-n) base
+        let in1_base = N + 1 + base;   // i+n+1+(l-n) base
+
+        for i in 0..=N - r {
+            let t_thing = (t_scale - t_vec[i + ir_base])
+                / (t_vec[i + in1_base] - t_vec[i + ir_base]);
+            temp[i] = t_thing * temp[i + 1] + (1.0 - t_thing) * temp[i];
+        }
+    }
+
+    return temp[0];
+}
+
+#[inline(always)]
+pub fn find_knot<const LEN: usize>(t: f32, t_vec: &Vec<f32>) -> usize {
+    //assume uniform knots
+
+    let t_scale = t * t_vec[t_vec.len() - 1];
+    let l = t_vec
+        .partition_point(|&knot| knot <= t_scale)
+        .saturating_sub(1).max(LEN-1). min(t_vec.len() - LEN - 2);
+    return l;
+}
+
+#[inline(always)] fn de_boors_derivative<const LEN: usize>(
+    control_points: &Vec<Vector2<f32>>,
+    t: f32,
+    t_vec: &Vec<f32>,
+    temp: &mut [Vector2<f32>; LEN],
+    l: usize) -> Vector2<f32> {
+
+    let p = LEN -1;
+    let N: usize = LEN - 2;
+    let t_scale = t * t_vec[t_vec.len() - 1];
+    let base = l - p;
+    for i in 0 ..=N{
+        let dt = t_vec[base + i + p] - t_vec[base + i];
+        temp[i] = (control_points[i + base + 1] - control_points[i + base]) * (p as f32/ dt)
+    }
+
+    for r in 1..=N {
+
+        let ir_base = r + base + 1;
+        let in1_base = N + 1 + base + 1; // i+n+1+(l-n) base
+
+        for i in 0..=N - r {
+            let t_thing = (t_scale - t_vec[i + ir_base]) / (t_vec[i + in1_base] - t_vec[i + ir_base]);
+            temp[i] = t_thing * temp[i + 1] + (1.0 - t_thing) * temp[i];
+        }
+    }
+    return temp[0];
+}
+
+#[inline(always)]
+fn de_boors_second_derivative<const LEN: usize>(control_points: &Vec<Vector2<f32>>, t: f32, t_vec: &Vec<f32>,temp: &mut [Vector2<f32>; LEN], l: usize) -> Vector2<f32> {
+
+    let p = LEN - 1;
+    let n1 = LEN - 3; // degree reduced by 2
+    let t_scale = t * t_vec[t_vec.len() - 1];
+    let base = l - p;
+
+
+    for i in 0..=n1 {
+        let dt1 = t_vec[base + i + p ] - t_vec[base + i + 1];
+        let dt0 = t_vec[base + i + p] - t_vec[base + i];
+        let cprime_i = (control_points[i + base + 1] - control_points[i + base]) * (p as f32 / dt0);
+        let cprime_next = (control_points[i + base + 2] - control_points[i + base + 1]) * (p as f32 / dt0);
+        temp[i] = (cprime_next - cprime_i) * ((p - 1) as f32 / dt1);
+    }
+
+    for r in 1..=n1 {
+        let ir_base = r + base + 2;        // i+r+(l-n) base
+        let in1_base = n1 + 1 + base + 2;   // i+n+1+(l-n) base
+
+        for i in 0..=n1 - r {
+            let t_thing = (t_scale - t_vec[i + ir_base])
+                / (t_vec[i + in1_base] - t_vec[i + ir_base]);
+            temp[i] = t_thing * temp[i + 1] + (1.0 - t_thing) * temp[i];
+        }
+    }
+
+    return temp[0];
+}
+
+fn render_spline(query: Query<(&Spline, &ControlledBy, &VisualizedBy)>,
+                 position_query: Query<&Position>,
+                 mut transforms: Query<&mut Transform>,
+){
+
+    let dim = 3;
+    let mut temp_buf: [Vector2<f32>; 4] = [Vector2::new(0.0, 0.0); 4];
+    for (spline, controlled_by, visualized_by) in &query {
+        let control_points = controlled_by.as_slice();
+
+        let positions: Vec<Vector2<f32>> = control_points
+            .iter()
+            .filter_map(|e| position_query.get(*e).ok())
+            .map(|p| p.0)
+            .collect();
+
+        let visual_points = visualized_by.as_slice();
+
+        let n = visual_points.len();
+        let step = 1.0 / n as f32;
+        let mut t = 0.0;
+
+
+        let mut v:Vec<f32> = Vec::with_capacity(positions.len() + dim + 1);
+        v.extend(std::iter::repeat(0.0).take(dim + 1)); // first n zeros
+        for i in 1..(positions.len() - dim ) {
+            v.push(i as f32);
+        }
+        v.extend(std::iter::repeat((positions.len() - dim) as f32).take(dim + 1)); // last n zeros
+
+
+
+
+        for i in 0..n {
+            // let point = de_casteljau(positions.clone(), t);
+
+            let l = find_knot::<4>(t, &v);
+            let point = de_boors::<4>(&positions, t, &v, &mut temp_buf, l);
+            if let Ok(mut transform) = transforms.get_mut(visual_points[i as usize]) {
+                transform.translation.x = point.x;
+                transform.translation.y = point.y;
+            }
+            t = t+step;
+
+
+        }
+
+    }
+}
+
+fn render_gradient(query: Query<(&Spline, &ControlledBy, &GradientVisualizedBy)>,
+                 position_query: Query<&Position, Without<VerletObject>>,
+                   mut object_query: Query<(&mut Position), (With<VerletObject>)>,
+                 mut transforms: Query<&mut Transform>,
+){
+
+    let dim = 3;
+    let mut temp_buf: [Vector2<f32>; 4] = [Vector2::new(0.0, 0.0); 4];
+
+    let Ok(object) = object_query.single() else {
+        return;
+    };
+    for (spline, controlled_by, visualized_by) in &query {
+        let control_points = controlled_by.as_slice();
+
+        let positions: Vec<Vector2<f32>> = control_points
+            .iter()
+            .filter_map(|e| position_query.get(*e).ok())
+            .map(|p| p.0)
+            .collect();
+
+        let visual_points = visualized_by.as_slice();
+
+        let n = visual_points.len();
+        let step = 1.0 / n as f32;
+
+
+        let mut v:Vec<f32> = Vec::with_capacity(positions.len() + dim + 1);
+        v.extend(std::iter::repeat(0.0).take(dim + 1)); // first n zeros
+        for i in 1..(positions.len() - dim ) {
+            v.push(i as f32);
+        }
+        v.extend(std::iter::repeat((positions.len() - dim) as f32).take(dim + 1)); // last n zeros
+
+
+
+        let mut t = get_nearest_spline_point(object.0, &positions);
+
+
+        for i in 0..n {
+            let l = find_knot::<4>(t, &v);
+            let point = de_boors::<4>(&positions, t, &v, &mut temp_buf, l);
+            let grad = de_boors_derivative::<4>(&positions, t, &v, &mut temp_buf, l);
+            // let dist_grad = (2.0 * (point - mouse.0).transpose() * grad);
+            if let Ok(mut transform) = transforms.get_mut(visual_points[i as usize]) {
+                transform.translation.x = point.x;
+                transform.translation.y = point.y;
+                // transform.scale = Vec3::splat(1.0) * (dist_grad.x * 0.001);
+
+                let angle = atan2(grad.y, grad.x);
+                transform.rotation = Quat::from_rotation_z(angle);
+            }
+            // t = t+step;
+
+
+        }
+        //
+        // let mut t = get_nearest_spline_point(object.0, &positions);
+        // let mut t = 0.0;
+        //
+        //
+        // for i in 0..n {
+        //     let l = find_knot::<4>(t, &v);
+        //
+        //     let f = de_boors::<4>(&positions, t, &v, &mut temp_buf, l);
+        //     let _df = de_boors_derivative::<4>(&positions, t, &v, &mut temp_buf, l);
+        //     let cached_ddf = de_boors_second_derivative::<4>(&positions, t, &v, &mut temp_buf, l);
+        //
+        //     // compute gradient and hessian (1D Newton)
+        //     let f_val = (f-object.0).norm_squared();
+        //     let df_val = (2.0 * (f - object.0).transpose() * _df).x;
+        //     let ddf_val = (2.0 * (_df.transpose() * _df + (f - object.0).transpose() * cached_ddf)).x;
+        //     // let dist_grad = (2.0 * (point - mouse.0).transpose() * grad);
+        //     if let Ok(mut transform) = transforms.get_mut(visual_points[i as usize]) {
+        //         transform.translation.x = f.x;
+        //         transform.translation.y = f.y;
+        //         // transform.scale = Vec3::splat(1.0) * (dist_grad.x * 0.001);
+        //
+        //         let angle = atan2(_df.y, _df.x);
+        //         transform.rotation = Quat::from_rotation_z(angle);
+        //     }
+        // t = t+step;
+        //
+        //
+        // }
+
+    }
+}
+
+pub fn get_nearest_spline_point(
+    point: Vector2<f32>,
+    positions: &Vec<Vector2<f32>>
+) -> f32{
+    let dim = 3;
+    let mut temp1: [Vector2<f32>; 4] = [Vector2::new(0.0, 0.0); 4];
+    let mut temp2 = [Vector2::zeros(); 4];
+
+
+        let mut v:Vec<f32> = Vec::with_capacity(positions.len() + dim + 1);
+        v.extend(std::iter::repeat(0.0).take(dim + 1)); // first n zeros
+        for i in 1..(positions.len() - dim ) {
+            v.push(i as f32);
+        }
+        v.extend(std::iter::repeat((positions.len() - dim) as f32).take(dim + 1)); // last n zeros
+
+
+
+   let mut l = 0usize;
+let mut f = Vector2::zeros();
+let mut _df = Vector2::zeros();
+let mut cached_ddf = Vector2::zeros();
+
+let mut t :f32= 0.5; // initial guess
+let tol = 1e-10;
+let max_iter = 10;
+    let mut rng = rand::thread_rng();
+
+let mut min_dist = f32::INFINITY;
+let mut min_t : f32 = 0.5;
+for i in 0..200{
+
+    t = (1.0 / 200.0) * i as f32;
+    l = find_knot::<4>(t, &v);
+    f = de_boors::<4>(&positions, t, &v, &mut temp1, l);
+    let dist = (f-point).norm_squared();
+
+    if(dist < min_dist){
+        min_dist = dist;
+        min_t = t;
+    }
+
+
+}
+    t = min_t;
+
+for _ in 0..max_iter {
+    t = t.min(1.0).max(0.0);
+    // compute everything once per t
+    l = find_knot::<4>(t, &v);
+
+    f = de_boors::<4>(&positions, t, &v, &mut temp1, l);
+    _df = de_boors_derivative::<4>(&positions, t, &v, &mut temp1, l);
+    cached_ddf = de_boors_second_derivative::<4>(&positions, t, &v, &mut temp2, l);
+
+    // compute gradient and hessian (1D Newton)
+    let df_val = (2.0 * (f - point).transpose() * _df).x;
+    let ddf_val = (2.0 * (_df.transpose() * _df + (f - point).transpose() * cached_ddf)).x;
+    if ddf_val.abs() < 1e-12 {
+        break; // avoid division by zero
+    }
+
+    let step = df_val / ddf_val;
+    t -= step / v[v.len() - 1];
+
+    if step.abs() < tol {
+        if((f - point).norm() > 100.0){
+            let t: f32 = rng.gen_range(0.0..1.0);
+            continue;
+        }
+        break;
+    }
+}
+
+    return t;
+
+
+
+}
